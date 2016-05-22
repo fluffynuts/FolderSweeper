3604863dcad9bf9f574cd7fb1c5b1b5f3dca90fb
Dave <davydm@gmail.com>
2016-05-22 17:14:52 +0200

    Minor tweaks

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 var sweeper = require('./src/folder-sweeper'),
     cli = require('cli');
+cli.enable('glob');
 cli.parse({
   'folder-jpg': ['j', 'Remove folder.jpg too'],
   'dry-run': ['d', 'Just report what would be done; don\'t actually do it']
@@ -9,6 +10,9 @@ cli.parse({
 cli.main((args, options) => {
   if (options['dry-run']) {
     console.log('Dry-run: will only log actions to be taken');
+  }
+  if (args.length === 0) {
+    console.log('no folders specified');
   }
   args.forEach(a => sweeper(a, options));
 });
